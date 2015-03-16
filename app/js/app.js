30f1e8bc902a11e02467853f72289156b389cb34
Tan Theng Lee <thengleetan@gmail.com>
2015-03-16 17:59:40 +0800

    Preview book review form part 2 - added genres

--- app/js/app.js
+++ app/js/app.js
@@ -13,8 +13,10 @@ angular.module('readingList', [])
 	.directive('bookGenres', function(){
 		return{
 			restrict: 'E',
-			templateUrl: 'partials/book-genres.html'
-
+			templateUrl: 'partials/book-genres.html',
+			scope: {
+				genres: '='
+			}
 		};
 	})
 
