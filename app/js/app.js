d94b0c3019311d386fc1e160b03e77b39d50c220
Tan Theng Lee <thengleetan@gmail.com>
2015-03-16 18:20:26 +0800

    Submit review form and added colored book ratings

--- app/js/app.js
+++ app/js/app.js
@@ -37,6 +37,14 @@ angular.module('readingList', [])
 			controller: function(){
 				this.showForm = false;
 				this.book = { genres: {} };
+
+				this.addReview = function(form){
+					books.push(this.book);
+					this.book = { genres: {} };
+
+					form.$setPristine();
+				}
+
 			},
 			controllerAs: 'reviewFormCtrl',
 			scope: {
